7d6efeb1ac884c155660b7d458226482e45e6ad2
xpy0035 <cristian.recalde.contractor@bbva.com>
2020-01-15 09:34:52 -0300

    Initial Commit

---
+++ Randog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randog
+//
+//  Created by Cristhian Jesus Recalde Franco on 1/15/20.
+//  Copyright © 2020 Cristhian Jesus Recalde Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
